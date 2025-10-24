cb35b7003257ddc1a4f7a179a23eb424f09c3023
Samuli Berg <samuli.berg@vare.fi>
2025-10-24 22:41:13 +0300

    navbar kuntoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import "bootswatch/dist/simplex/bootstrap.min.css";
 import Schedule from './schedule.jsx';
 import Scores from './score.jsx';
@@ -10,47 +11,37 @@ import Standings from './standings.jsx';
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg bg-success navbar-dark">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">
-          <img
-            src="https://assets.nhle.com/logos/nhl/svg/NHL_light.svg"
-            alt="NHL Logo"
-            height="40"
-            style={{ filter: 'invert(0)' }} // Optional: makes it visible on dark background
-          />
-          </NavLink>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/schedule">Schedule</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/scores">Scores</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/stats">Stats</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/standings">Standings</NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar expand="lg" bg="success" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="/#/">
+            <img
+              src="https://assets.nhle.com/logos/nhl/svg/NHL_light.svg"
+              alt="NHL Logo"
+              height="40"
+              style={{ filter: 'invert(0)' }}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/#/scores">Scores</Nav.Link>
+              <Nav.Link href="/#/schedule">Schedule</Nav.Link>
+              <Nav.Link href="/#/stats">Stats</Nav.Link>
+              <Nav.Link href="/#/standings">Standings</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <div className="container mt-4">
+      <Container className="mt-4">
         <Routes>
-          <Route path="/" element={<Schedule />} />
+          <Route path="/" element={<Scores />} />
           <Route path="/schedule" element={<Schedule />} />
           <Route path="/scores" element={<Scores />} />
           <Route path="/stats" element={<Stats />} />
           <Route path="/standings" element={<Standings />} />
         </Routes>
-      </div>
+      </Container>
     </Router>
   );
 }
